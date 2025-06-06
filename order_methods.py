b68de97af1b832eb6f68602cda4904931cd7897f
Sergei Filipchenko <s.e.r.e.g.a.-93@mail.ru>
2025-06-06 19:21:04 +0500

    Fix

--- order_methods.py
+++ order_methods.py
@@ -6,13 +6,13 @@ from data import Url
 class OrderMethods:
 
     @staticmethod
-    @allure.title('Создание заказа')
+    @allure.step('Создание заказа')
     def created_order(body, token):
         header = {"Authorization": token}
         return requests.post(f'{Url.BASE_URL}{Url.CREATED_ORDER}', headers=header, json=body)
 
     @staticmethod
-    @allure.title('Получение заказов пользователя')
+    @allure.step('Получение заказов пользователя')
     def get_orders_user(token):
         header = {"Authorization": token}
         return requests.get(f'{Url.BASE_URL}{Url.GET_ORDERS_USER}', headers=header)
--- user_methods.py
+++ user_methods.py
@@ -6,23 +6,23 @@ from data import Url
 class UserMethods:
 
     @staticmethod
-    @allure.title('Создание пользователя')
+    @allure.step('Создание пользователя')
     def created_user(body):
         return requests.post(f'{Url.BASE_URL}{Url.CREATED_USER}', json=body)
 
     @staticmethod
-    @allure.title('Авторизация пользователем')
+    @allure.step('Авторизация пользователем')
     def login_user(body):
         return requests.post(f'{Url.BASE_URL}{Url.LOGIN_USER}', json=body)
 
     @staticmethod
-    @allure.title('Обновление данных пользователя')
+    @allure.step('Обновление данных пользователя')
     def update_user_data(body, token):
         header = { "Authorization": token }
         return requests.patch(f'{Url.BASE_URL}{Url.UPDATE_USER}', headers=header, json=body)
 
     @staticmethod
-    @allure.title('Удаление пользователя')
+    @allure.step('Удаление пользователя')
     def delete_user(token):
         header = {"Authorization": token}
         return requests.delete(f'{Url.BASE_URL}{Url.DELETE_USER}', headers=header)
